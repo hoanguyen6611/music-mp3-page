3fdc03f74743fe8601935beb6509d3363a9a8bbd
hoa <hoahuy2606@gmail.com>
2022-12-07 15:00:24 +0700

    update deploy

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCgbBGWHPBUr6_pZgKuT1NuQvJtazl2WdY',
+    authDomain: 'music-mp3-page.firebaseapp.com',
+    projectId: 'music-mp3-page',
+    storageBucket: 'music-mp3-page.appspot.com',
+    messagingSenderId: '767410924896',
+    appId: '1:767410924896:web:0f8af1c7d21321e8638205',
+    measurementId: 'G-GN06H3ZFRZ',
+  },
+  urlBE: 'http://localhost:5000',
+  client_Id: '767410924896-44ub0pa56icd7uu7fpsr1h94n9k9a2ll.apps.googleusercontent.com'
+};
+
